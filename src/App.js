875a08eb4c907ca046a7754ddeabacdfb4a76c89
Suhyun56 <sh.lee@snaptag.co.kr>
2022-09-13 10:27:14 +0900

    라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Router } from "react-router-dom";
+import CreateRoutes from "./routes/Routes";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <CreateRoutes />
+      </Router>
     </div>
   );
 }
