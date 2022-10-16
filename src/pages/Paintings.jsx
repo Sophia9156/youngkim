4e652e5fcb5edd72552b8726e0c721e761bb1aa1
Sophia <91585668+Sophia9156@users.noreply.github.com>
2022-10-17 00:20:56 +0900

    paintings description 클릭 시 표시로 변경

--- src/pages/Paintings.jsx
+++ src/pages/Paintings.jsx
@@ -5,11 +5,35 @@ import { useEffect, useState } from "react";
 export default function Paintings() {
   const [intro, setIntro] = useState({});
   const [paintings, setPaintings] = useState([]);
+  const [isCaptionOpen, setCaptionOpen] = useState([]);
 
   useEffect(() => {
     setIntro(json.intro);
     setPaintings(json.paintings);
+    let arr = [];
+    json.paintings.forEach(painting => {
+      arr.push({id: painting.id, isOpen: false});
+    });
+    setCaptionOpen(arr);
   }, []);
+
+  const handleClick = (id) => {
+    let arr = [];
+    if(isCaptionOpen.find(el => el.id === id).isOpen === true) {
+      json.paintings.forEach(painting => {
+        arr.push({id: painting.id, isOpen: false});
+      })
+    } else {
+      json.paintings.forEach(painting => {
+        if(painting.id === id) {
+          arr.push({id: painting.id, isOpen: true});
+        } else {
+          arr.push({id: painting.id, isOpen: false});
+        }
+      })
+    }
+    setCaptionOpen(arr);
+  }
 
   return (
     <main>
@@ -37,13 +61,17 @@ export default function Paintings() {
         <ul className="paintings-list">
           {paintings.map(painting => (
             <li key={painting.id} className="paintings-list-item">
-              <figure className="painting-image">
+              <figure className="painting-image"
+                onClick={() => handleClick(painting.id)}
+              >
                 <img src={painting.image} alt="painting" />
+                {isCaptionOpen.find(el => el.id === painting.id).isOpen && (
+                  <figcaption className="painting-caption">
+                    {painting.description}
+                  </figcaption>
+                )}
               </figure>
               <h5 className="painting-title">{painting.title}</h5>
-              <p className="painting-description">
-                {painting.description}
-              </p>
             </li>
           ))}
         </ul>
